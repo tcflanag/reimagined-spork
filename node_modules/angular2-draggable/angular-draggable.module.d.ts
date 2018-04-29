e16c58d6460d064399afad2831dbc2721ea12f69
Chris Flanagan <tiwato@gmail.com>
2018-04-28 19:38:16 -0700

    Initial commit

---
+++ node_modules/angular2-draggable/angular-draggable.module.d.ts
@@ -0,0 +1,4 @@
+import { AngularDraggableDirective, IPosition } from './directive/angular-draggable.directive';
+export declare class AngularDraggableModule {
+}
+export { AngularDraggableDirective, IPosition };
